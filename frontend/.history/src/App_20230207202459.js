b9f2d0cfa141d14b6c59732811b427a06ec179c5
LolaSL <lolashepota@gmail.com>
2023-02-07 20:46:01 +0200

    Create react application demo-blogt

---
+++ frontend/.history/src/App_20230207202459.js
@@ -0,0 +1,15 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+<a href = "/">Demo 
+<button type="button" class="btn btn-lg btn-warning">button</button>
+</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/.history/src/App_20230207202515.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+<a href = "/">Demo 
+<button type="button" class="btn btn-lg btn-warning">button</button>
+</a>
+      </header>
+      <main></main>
+    </div>
+  );
+}
+
+export default App;
